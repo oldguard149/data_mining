8bb37201eebbaa3fc6d6b20afb7146f47c54bf9a
Vinh Thuan <tvthuan1409@gmail.com>
2020-11-15 12:35:04 +0700

    Stable edition

--- routes/resultdata/crontab_for_delete_file.js
+++ routes/resultdata/crontab_for_delete_file.js
@@ -2,9 +2,7 @@ const CronJob = require('cron').CronJob;
 const fs = require('fs');
 const path = require('path');
 
-const job = new CronJob('0 */10 * * * *', function () {
-    console.log(Date())
-    console.log('check result csv file for delete');
+const job = new CronJob('0 */1 * * * *', function () {
     fs.readdirSync(__dirname)
         .filter(file => file !== 'test.js' && file !== 'cron_for_delete_file.js')
         .map(file => file.split('.')[0])
